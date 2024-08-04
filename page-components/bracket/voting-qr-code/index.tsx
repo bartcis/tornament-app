511188cabb19606de51420abb191422218956c51
Bartek <>
2024-08-04 23:02:00 +0200

    remove npm lock file

--- page-components/bracket/voting-qr-code/index.tsx
+++ page-components/bracket/voting-qr-code/index.tsx
@@ -1,16 +1,24 @@
 "use client";
 
+import { useCallback } from "react";
 import QRCode from "react-qr-code";
 
 export const VotingQRCode = () => {
-  console.log();
+  const generateUrl = useCallback(() => {
+    try {
+      return `${window.location.origin}/voting`;
+    } catch {
+      return "No url";
+    }
+  }, []);
+
   return (
     <div className="absolute right-8 top-8">
       <p>Link do głosowania:</p>
       <QRCode
         size={256}
         style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-        value={`${window.location.origin}/voting`}
+        value={generateUrl()}
         viewBox={`0 0 256 256`}
       />
     </div>
